0adeca888e712b5c73076c66731bae2ac027855e
egodolja <elsi5@vt.edu>
2025-10-16 18:57:12 -0400

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+export default defineConfig({
+  plugins: [react()],
   base: "/egodolja.github.io",
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-}));
+});
